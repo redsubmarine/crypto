127e5d6cfe65b3963ec06fc4579777dfa6006bdc
red <wonseok@duck.com>
2023-03-10 19:38:12 +0900

    Mock 데이터를 이용해서 CoinRowView 레이아웃 추가.

---
+++ CryptoNick/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,74 @@
+//
+//  CoinRowView.swift
+//  CryptoNick
+//
+//  Created by 레드 on 2023/03/10.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin: Coin
+    let showHoldingsColumn: Bool
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            leftColumn
+            Spacer()
+            
+            if showHoldingsColumn {
+                centerColumn
+            }
+            
+            rightColumn
+        }
+        .font(.subheadline)
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin, showHoldingsColumn: true)
+            .previewLayout(.sizeThatFits)
+            .preferredColorScheme(.dark)
+            
+    }
+}
+
+extension CoinRowView {
+    private var leftColumn: some View {
+        HStack(spacing: 0) {
+            Text(coin.rank.description)
+                .font(.caption)
+                .foregroundColor(.theme.secondaryText)
+                .frame(minWidth: 30)
+            Circle()
+                .frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased())
+                .font(.headline)
+                .padding(.leading, 6)
+                .foregroundColor(.theme.accent)
+        }
+    }
+    private var centerColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentHoldingsValue.asCurrencyWith2Decimals())
+            Text((coin.currentHoldings ?? 0).asNumberString())
+        }
+            .foregroundColor(.theme.accent)
+    }
+    
+    private var rightColumn: some View {
+        VStack(alignment: .trailing) {
+            Text(coin.currentPrice.asCurrencyWith6Decimals())
+                .bold()
+                .foregroundColor(.theme.accent)
+            Text(coin.priceChangePercentage24H?.asPercentString() ?? "")
+                .foregroundColor(
+                    (coin.priceChangePercentage24H ?? 0) >= 0 ?
+                .theme.green : .theme.red
+            )
+        }
+        .frame(width: UIScreen.main.bounds.width / 3.5, alignment: .trailing)
+    }
+}
---
+++ CryptoNick/Extensions/Double.swift
@@ -0,0 +1,78 @@
+//
+//  Double.swift
+//  CryptoNick
+//
+//  Created by 레드 on 2023/03/10.
+//
+
+import Foundation
+
+extension Double {
+    /// Converts a Double into a Currency with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to $1,234.56
+    /// ```
+    private var currencyFormatter2: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        formatter.currencyCode = "usd"
+        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 2
+        return formatter
+    }
+    
+    /// Converts a Double into a String with 2 decimal places
+    /// ```
+    /// Convert 1234.56 to "$1,234.56"
+    /// ```
+    func asCurrencyWith2Decimals() -> String {
+        currencyFormatter2.string(from: .init(value: self)) ?? "$0.00"
+    }
+    
+    /// Converts a Double into a Currency with 2-6 decimal places
+    /// ```
+    /// Convert 1234.56 to $1,234.56
+    /// Convert 12.3456 to $12.3456
+    /// Convert 0.123456 to $0.123456
+    /// ```
+    private var currencyFormatter6: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.usesGroupingSeparator = true
+        formatter.numberStyle = .currency
+        formatter.locale = .current
+        formatter.currencyCode = "usd"
+        formatter.currencySymbol = "$"
+        formatter.minimumFractionDigits = 2
+        formatter.maximumFractionDigits = 6
+        return formatter
+    }
+    
+    /// Converts a Double into a String with 2-6 decimal places
+    /// ```
+    /// Convert 1234.56 to "$1,234.56"
+    /// Convert 12.3456 to "$12.3456"
+    /// Convert 0.123456 to "$0.123456"
+    /// ```
+    func asCurrencyWith6Decimals() -> String {
+        currencyFormatter6.string(from: .init(value: self)) ?? "$0.00"
+    }
+    
+    /// Converts a Double into string  representation
+    /// ```
+    /// Convert 1.2345 to "1.23"
+    /// ```
+    func asNumberString() -> String {
+        String(format: "%.2f", self)
+    }
+    
+    /// Converts a Double into string  representation
+    /// ```
+    /// Convert 1.2345 to "1.23%"
+    /// ```
+    func asPercentString() -> String {
+        asNumberString() + "%"
+    }
+}
