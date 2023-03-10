5496d86f4a5bd68b875bd6f77031e1b05cb8f8d8
red <wonseok@duck.com>
2023-03-10 16:43:54 +0900

    Initial Commit

---
+++ CryptoNick/CryptoNickApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoNickApp.swift
+//  CryptoNick
+//
+//  Created by 레드 on 2023/03/10.
+//
+
+import SwiftUI
+
+@main
+struct CryptoNickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
